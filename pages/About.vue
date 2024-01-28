698990a0129b7d3cfd413e54579cca7ecb117f8f
Ákos Kappel <kappelakos@gmail.com>
2024-01-28 22:01:52 +0100

    add about page

--- pages/About.vue
+++ pages/About.vue
@@ -1,6 +1,46 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold text-center">About</h1>
-    <p class="text-center">This is the about page</p>
+  <div class="container mx-auto px-4 py-8">
+    <h1 class="text-4xl font-bold text-center mb-6">About This Project</h1>
+    <div class="bg-white rounded-lg shadow-lg p-6 md:p-12">
+      <p class="text-lg text-gray-700">
+        This project is a simple web application that uses the
+        <a
+          href="https://www.themealdb.com/api.php"
+          class="text-blue-500 hover:underline"
+          target="_blank"
+          rel="noopener noreferrer"
+          >MealDB API
+        </a>
+        to display recipes and ingredients.
+      </p>
+      <p class="text-lg text-gray-700 mt-4">It was built using the following technologies:</p>
+      <ul class="list-disc list-inside text-lg text-gray-700 mt-4">
+        <li>
+          <a href="https://nuxt.com/" class="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer"
+            >Nuxt.js
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://tailwindcss.com/"
+            class="text-blue-500 hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tailwind CSS
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://axios-http.com/"
+            class="text-blue-500 hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Axios
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>

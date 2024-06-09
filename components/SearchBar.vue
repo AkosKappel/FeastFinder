92c89d0b0c942126bd2d232bbf02c43858766472
Ákos Kappel <kappelakos@gmail.com>
2024-06-09 20:10:58 +0200

    small footer and search updates

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -6,7 +6,7 @@
         type="text"
         id="simple-search"
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
-        placeholder="Search for a meal..."
+        placeholder="E.g. Chicken, Pasta, Vegan..."
         required
         v-model="input"
         @keyup.enter.prevent="search(input)"
